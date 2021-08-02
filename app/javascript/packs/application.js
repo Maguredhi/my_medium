f87e2205afc5df0f85c455ef62942873e00ce876
maguredhi <genso1104@gmail.com>
2021-08-02 16:47:42 +0800

    add stimulus and foreman

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "styles/frontend"
+import "controllers"
 
 Rails.start()
 Turbolinks.start()
