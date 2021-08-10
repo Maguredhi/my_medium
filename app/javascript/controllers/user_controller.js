8d83b8090437ae38f50286f830b4a2debc49110c
maguredhi <genso1104@gmail.com>
2021-08-10 14:45:03 +0800

    1. add bookmark model 2. finish bookmark

--- app/javascript/controllers/user_controller.js
+++ app/javascript/controllers/user_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import axios from "axios"
 
 export default class extends Controller {
-  static targets = [ "followButton" ]
+  static targets = [ "followButton", "bookmark" ]
 
   follow(event) {
     event.preventDefault()
@@ -11,20 +11,49 @@ export default class extends Controller {
     let button = this.followButtonTarget
 
     axios.post(`/api/users/${userId}/follow`)
-    .then(function (response) {
-      let status = response.data.status
-      switch (status) {
-        case 'sign_in_first':
-          alert('必須先登入')
-          break;
-        default:
-          button.innerHTML = status
-          break;
-      }
-      console.log(response.data)
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
+         .then(function (response) {
+           let status = response.data.status
+           switch (status) {
+             case 'sign_in_first':
+               alert('必須先登入')
+               break;
+             default:
+               button.innerHTML = status
+               break;
+           }
+           console.log(response.data)
+         })
+         .catch(function (error) {
+           console.log(error)
+         })
+  }
+
+  bookmark(event){
+    event.preventDefault()
+
+    let link = event.currentTarget
+    let slug = link.dataset.slug
+    let icon = this.bookmarkTarget
+
+    axios.post(`/api/stories/${slug}/bookmark`)
+         .then(function (response) {
+           switch (response.data.status) {
+             case 'Bookmarked':
+               // fontawesome icon class
+               icon.classList.add('fas')
+               icon.classList.remove('far')
+               break;
+             case 'Unbookmark':
+               icon.classList.add('far')
+               icon.classList.remove('fas')
+               break;
+           }
+           console.log(response.data)
+         })
+         .catch(function (error) {
+           console.log(link)
+           console.log(slug)
+           console.log(error)
+         })
   }
 }
